26e2d0f32d5de0974d0780b97c383b365db674ba
Daniel Sousa <mail@danielsousa.io>
2020-09-05 00:22:21 +0100

    Update screen

--- src/features/BlankScreen112/screens/blank.js
+++ src/features/BlankScreen112/screens/blank.js
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { View, Image, ImageBackground } from "react-native";
+import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input, Datepicker } from 'react-native-ui-kitten';
+import Slider from '@react-native-community/slider';
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
-export class Blank extends Component {
-  render() {
-    return (
-      <View>Hi Charath! This is the player.</View>
-    )
-  }
+export class _Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
+    };
+  };
+
+  state = { }
+
+  render = () => <View style={this.props.themedStyle.container}>Hi Charath! This is the player.</View>;
+  
 }
 
-AppRegistry.registerComponent('App', () => Blank);
+export default Blank = withStyles(_Blank, theme => ({
+  container: {
+    backgroundColor: theme["color-basic-100"]
+  }
+}));
